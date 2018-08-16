37f3336547191728bfcd34ce2f45fce18f366841
LYoung05 <yuanliyong520@126.com>
2018-08-16 10:21:43 +0800

    Initial Commit

---
+++ LYWeChatMaker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LYWeChatMaker
+//
+//  Created by CNFOL_iOS on 2018/8/16.
+//  Copyright © 2018年 LYoung_iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
